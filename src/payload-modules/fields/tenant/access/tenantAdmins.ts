e8c815d91f0c1c492162d4b398dabbed500e0fd5
Djboy08 <justindoane@gmail.com>
2024-05-23 15:21:50 -0400

    test fix

--- src/payload-modules/fields/tenant/access/tenantAdmins.ts
+++ src/payload-modules/fields/tenant/access/tenantAdmins.ts
@@ -4,20 +4,19 @@ import { checkUserRoles } from '../../../utilities/checkUserRoles'
 import { Tenant, User } from '~/payload-types'
 
 export const tenantAdminFieldAccess: FieldAccess = ({ req: { user }, doc }) => {
-  return (
-    checkUserRoles(['super-admin'], user) ||
-    !doc?.tenant ||
-    (doc?.tenant &&
-      user?.tenants?.some(
-        ({
-          tenant: userTenant,
-          roles,
-        }: {
-          tenant: NonNullable<User['tenants']>[0]['tenant']
-          roles: NonNullable<User['tenants']>[0]['roles']
-        }) =>
-          (typeof doc?.tenant === 'string' ? doc?.tenant : doc?.tenant.id) ===
-            (userTenant as Tenant)?.id && roles?.includes('admin'),
-      ))
-  )
+  return checkUserRoles(['super-admin'], user)
+  // ||
+  // !doc?.tenant ||
+  // (doc?.tenant &&
+  //   user?.tenants?.some(
+  //     ({
+  //       tenant: userTenant,
+  //       roles,
+  //     }: {
+  //       tenant: NonNullable<User['tenants']>[0]['tenant']
+  //       roles: NonNullable<User['tenants']>[0]['roles']
+  //     }) =>
+  //       (typeof doc?.tenant === 'string' ? doc?.tenant : doc?.tenant.id) ===
+  //         (userTenant as Tenant)?.id && roles?.includes('admin'),
+  //   ))
 }
